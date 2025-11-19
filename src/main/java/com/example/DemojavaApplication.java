f1c9b7f7485de9776eeb52b1dfa38d9ec3c38a9c
Venkat-Expleo <venkatesan.babu@expleogroup.com>
2025-11-19 16:35:23 +0530

    Create DemojavaApplication.java

---
+++ src/main/java/com/example/DemojavaApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemojavaApplication.class, args);
+    }
+}
